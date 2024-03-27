bd5957abd077d0e1c65757c0baeebab4f5c016ba
Nayeem <nayeem2281998@gmail.com>
2024-03-27 11:09:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex h-screen'>
+      <div style={{backgroundColor:'#0A0E2B'}}>heyy</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
